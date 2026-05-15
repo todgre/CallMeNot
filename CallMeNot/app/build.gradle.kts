205c8f93d98b219eabf3849ac27236c507e3e8cd
todgre <todgre@gmail.com>
2026-05-15 14:47:37 -0500

    Bump compileSdk and targetSdk to 35 (required by Play Console)

--- CallMeNot/app/build.gradle.kts
+++ CallMeNot/app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.callmenot.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.callmenot.app"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
