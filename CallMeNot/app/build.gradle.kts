32de12a488f73d2b553d656070d5fbda4891a986
todgre <todgre@gmail.com>
2026-05-19 14:49:01 -0500

    Bump versionCode to 3, versionName to 1.0.1

--- CallMeNot/app/build.gradle.kts
+++ CallMeNot/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.callmenot.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
