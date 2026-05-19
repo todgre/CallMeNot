128d790d8dcde8f6793cc20cd90cfafab985d557
todgre <todgre@gmail.com>
2026-05-19 15:36:33 -0500

    Remove WorkManager from Application class

--- CallMeNot/app/src/main/java/com/callmenot/app/CallMeNotApp.kt
+++ CallMeNot/app/src/main/java/com/callmenot/app/CallMeNotApp.kt
@@ -2,31 +2,15 @@ package com.callmenot.app
 
 import android.app.Application
 import android.util.Log
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class CallMeNotApp : Application(), Configuration.Provider {
+class CallMeNotApp : Application() {
 
     companion object {
         private const val TAG = "CallMeNotApp"
     }
 
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override val workManagerConfiguration: Configuration
-        get() = try {
-            Configuration.Builder()
-                .setWorkerFactory(workerFactory)
-                .build()
-        } catch (e: Exception) {
-            Log.e(TAG, "Error creating WorkManager config", e)
-            Configuration.Builder().build()
-        }
-    
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "CallMeNotApp initialized")
